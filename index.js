87039f902e001cf858797b8162feaa9207ae4f25
HARSHIT JAIN <hsancheti45@HARSHITs-MacBook-Air.local>
2021-07-14 14:06:05 +0530

    final updated

--- index.js
+++ index.js
@@ -3,15 +3,9 @@ const app = express();
 const path = require("path");
 const connect = require("./db/connect");
 const Details = require("./db/userSchema");
-<<<<<<< HEAD
-
-connect();
-app.listen(3000, () => console.log("Server started at 3000"));
-=======
 const Doctor = require("./db/doctorSchema");
 const port = process.env.PORT || 80;
 connect();
->>>>>>> Harshit
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -23,60 +17,6 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-<<<<<<< HEAD
-  res.sendFile(path.join(__dirname + "/form.html"));
-});
-
-app.post("/details", (req, res) => {
-  console.log(req.body);
-  const {
-    sl_no,
-    id_title,
-    id_first_name,
-    id_last_name,
-    id_sex,
-    id_age,
-    id_email,
-    id_phone,
-    id_address,
-    id_type_dm,
-    id_duration,
-    id_date_of_diabetes,
-    id_treatment,
-    blood_sugar_level,
-    FBS,
-    PPBS,
-    Glycosylated_hb,
-    plaque_index,
-  } = req.body;
-
-  const details = new Details({
-    // sl_no,
-    // title: id_title,
-    // first_name: id_first_name,
-    // last_name: id_last_name,
-    // sex: id_sex,
-    // age: id_age,
-    // email: id_email,
-    // phone: id_phone,
-    // address: id_address,
-    // type_dm: id_type_dm,
-    // duration: id_duration,
-    // date_of_diabetes: id_date_of_diabetes,
-    // treatment: id_treatment,
-    // blood_sugar_level: blood_sugar_level,
-    // FBS: FBS,
-    // PPBS: PPBS,
-    // Glycosylated_hb: Glycosylated_hb,
-    plaque_index,
-  });
-  details
-    .save()
-    .then((result) => console.log("Data saved"))
-    .catch((err) => console.log(err));
-  res.json({ msg: "Thank you" });
-});
-=======
   
   res.sendFile(path.join(__dirname + "/login.html"));
 });
@@ -210,4 +150,3 @@ app.post("/sign-in", (req, res) => {
     });
   });
   app.listen(port,() => console.log(`Server started ${port}`));
->>>>>>> Harshit
