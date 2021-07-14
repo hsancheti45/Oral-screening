99593be1d8933ddfb7f1eaa8d32d5e307893ee6b
HARSHIT JAIN <hsancheti45@HARSHITs-MacBook-Air.local>
2021-07-14 13:47:42 +0530

    hosting

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const path = require("path");
 const connect = require("./db/connect");
 const Details = require("./db/userSchema");
 const Doctor = require("./db/doctorSchema");
-// const port = process.env.PORT || 80;
+const port = process.env.PORT || 80;
 connect();
-app.listen(process.env.PORT,process.env.IP,() => console.log(`Server started at ${port}`));
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -52,101 +51,102 @@ app.post("/sign-up", (req, res) => {
   doctor
   .save()
   .then((result) => console.log("Doctor sign up details saved"))
-    .catch((err) => console.error("Error"));
-    res.json({ msg: "Data saved" });
+  .catch((err) => console.error("Error"));
+  res.json({ msg: "Data saved" });
+});
+
+app.post("/sign-in", (req, res) => {
+  console.log(req.body);
+  Doctor.findOne(
+    { username: req.body.username, password: req.body.password },
+    function (err, result) {
+      if (err) res.json({ msg: "Failed to get data" });
+      else res.json(result);
+    }
+    );
   });
   
-  app.post("/sign-in", (req, res) => {
+  app.post("/details", (req, res) => {
     console.log(req.body);
-    Doctor.findOne(
-      { username: req.body.username, password: req.body.password },
-      function (err, result) {
-        if (err) res.json({ msg: "Failed to get data" });
-        else res.json(result);
+    const {
+      sl_no,
+      id_title,
+      id_first_name,
+      id_last_name,
+      id_sex,
+      id_age,
+      id_email,
+      id_phone,
+      id_address,
+      id_type_dm,
+      id_duration,
+      id_date_of_diabetes,
+      id_treatment,
+      blood_sugar_level,
+      FBS,
+      PPBS,
+      Associated_Systemic_Problems,
+      Diabetic_complications,
+      
+      Glycosylated_hb,
+      plaque_index,
+      gingival_index,
+      probing_pocket,
+      clinical_attached_levels,
+      probing_pocket_depth,
+      clinical_attachment_levels,
+      
+      pufa_index,
+      dmft_index,
+      mucosal,
+      question,
+    } = req.body;
+    
+    Details.findOne({slno: sl_no}, function(err, data){
+      if(err){
+        res.json({msg: "Failed"});
       }
-  );
-});
-
-app.post("/details", (req, res) => {
-  console.log(req.body);
-  const {
-    sl_no,
-    id_title,
-    id_first_name,
-    id_last_name,
-    id_sex,
-    id_age,
-    id_email,
-    id_phone,
-    id_address,
-    id_type_dm,
-    id_duration,
-    id_date_of_diabetes,
-    id_treatment,
-    blood_sugar_level,
-    FBS,
-    PPBS,
-    Associated_Systemic_Problems,
-    Diabetic_complications,
-    
-    Glycosylated_hb,
-    plaque_index,
-    gingival_index,
-    probing_pocket,
-    clinical_attached_levels,
-    probing_pocket_depth,
-    clinical_attachment_levels,
-    
-    pufa_index,
-    dmft_index,
-    mucosal,
-    question,
-  } = req.body;
-
-  Details.findOne({slno: sl_no}, function(err, data){
-    if(err){
-      res.json({msg: "Failed"});
-    }
-    if(data === null){
-      const details = new Details({
-        slno: sl_no,
-        title: id_title,
-        first_name: id_first_name,
-        last_name: id_last_name,
-        sex: id_sex,
-        age: id_age,
-        email: id_email,
-        phone: id_phone,
-        address: id_address,
-        type_dm: id_type_dm,
-        duration: id_duration,
-        date_of_diabetes: id_date_of_diabetes,
-        treatment: id_treatment,
-    blood_sugar_level: blood_sugar_level,
-    FBS: FBS,
-    PPBS: PPBS,
-    Diabetic_complications: Diabetic_complications,
-    Associated_Systemic_Problems: Associated_Systemic_Problems,
-    Glycosylated_hb: Glycosylated_hb,
-    plaque_index,
-    gingival_index,
-    probing_pocket,
-    clinical_attached_levels,
-    probing_pocket_depth,
-    clinical_attachment_levels,
-    mucosal,
-    pufa_index,
-    dmft_index,
-    question,
+      if(data === null){
+        const details = new Details({
+          slno: sl_no,
+          title: id_title,
+          first_name: id_first_name,
+          last_name: id_last_name,
+          sex: id_sex,
+          age: id_age,
+          email: id_email,
+          phone: id_phone,
+          address: id_address,
+          type_dm: id_type_dm,
+          duration: id_duration,
+          date_of_diabetes: id_date_of_diabetes,
+          treatment: id_treatment,
+          blood_sugar_level: blood_sugar_level,
+          FBS: FBS,
+          PPBS: PPBS,
+          Diabetic_complications: Diabetic_complications,
+          Associated_Systemic_Problems: Associated_Systemic_Problems,
+          Glycosylated_hb: Glycosylated_hb,
+          plaque_index,
+          gingival_index,
+          probing_pocket,
+          clinical_attached_levels,
+          probing_pocket_depth,
+          clinical_attachment_levels,
+          mucosal,
+          pufa_index,
+          dmft_index,
+          question,
+        });
+        details
+        .save()
+        .then((result) => console.log("Data saved"))
+        .catch((err) => console.log(err));
+        res.json({ msg: "Thank you" });
+      }
+      else{
+        res.json({msg: "Sl_no already exists"});
+      }
+    });
   });
-  details
-  .save()
-  .then((result) => console.log("Data saved"))
-    .catch((err) => console.log(err));
-  res.json({ msg: "Thank you" });
-    }
-    else{
-      res.json({msg: "Sl_no already exists"});
-    }
-  });
-});
+  app.listen(port,() => console.log(`Server started ${port}`));
