5037704725d7c32adccf4a16ff22cf6097b47df0
HARSHIT JAIN <hsancheti45@HARSHITs-MacBook-Air.local>
2021-07-14 20:19:25 +0530

    vercel added

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ app.listen(port,() => console.log(`Server started ${port}`));
 app.use(express.json());
 
 app.use((req, res, next) => {
+
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://oral-screening-hsancheti45.vercel.app/");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
